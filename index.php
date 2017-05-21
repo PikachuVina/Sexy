f6a5301b1e482a510e71f7ed41eeabb5a37320bf
PikachuVina <bmn.2312@gmail.com>
2017-05-21 20:09:40 +0700

    1

--- index.php
+++ index.php
@@ -225,7 +225,7 @@ if($_GET['ref']){
 								?>
 								</div>
 								<div class="box-footer">
-									<center>Hiện Tại Có <span class="label label-default"><?php echo mysqli_num_rows(mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM `vip` ORDER BY RAND()")); ?></span> Người Dùng VIP Trên Hệ Thống.</center>
+									<center>Hiện Tại Có <b><?php echo mysqli_num_rows(mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM `vip` ORDER BY RAND()")); ?></b> Người Dùng VIP Trên Hệ Thống.</center>
 								</div>
                                 </div>
                             </div>
@@ -265,7 +265,7 @@ if($_GET['ref']){
 								?>
 								</div>
 								<div class="box-footer">
-									<center>Hiện Tại Có <span class="label label-default"><?php echo mysqli_num_rows(mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM `taikhoan` ORDER BY RAND()")); ?></span> Thành Viên Trên Hệ Thống.</center>
+									<center>Hiện Tại Có <b><?php echo mysqli_num_rows(mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM `taikhoan` ORDER BY RAND()")); ?></b> Thành Viên Trên Hệ Thống.</center>
 								</div>
                                 </div>
                             </div>
